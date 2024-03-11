814be13da5435731d432173132da2b0efd9bbae2
xiracuko <matvey.a.a.7@gmail.com>
2024-03-11 03:27:57 +0300

    refactor: added react-router library to main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './scss/styles.scss';
 
 function App() {
 
   return (
-    <div className="app"></div>
+    <div className="app">
+      <Routes>
+        <Route path='/*'>
+          <Route path='home' />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
