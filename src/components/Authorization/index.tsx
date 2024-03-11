bdd945cc956b718d201a040d4b4bf38fceb858e8
xiracuko <matvey.a.a.7@gmail.com>
2024-03-11 04:29:16 +0300

    build: added second input and button to form

--- src/components/Authorization/index.tsx
+++ src/components/Authorization/index.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import styles from './Authorization.module.scss';
 
 function Authorization() {
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
   const onSubmit = (data: any) => console.log(data);
 
   return (
@@ -10,8 +10,12 @@ function Authorization() {
       <div className={styles.authBlock}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <input type="text" {...register('name', {
+            
+          })} />
+          <input type="text" {...register('email', {
 
           })} />
+          <button>Submit</button>
         </form>
       </div>
     </div>
