f8dd477bbcb6bea5fa0fc74a1757f7b31275f06f
xiracuko <matvey.a.a.7@gmail.com>
2024-03-11 03:31:35 +0300

    build: added components to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './scss/styles.scss';
+import Authorization from './components/Authorization';
+import Home from './components/Home';
 
 function App() {
 
   return (
     <div className="app">
       <Routes>
-        <Route path='/*'>
-          <Route path='home' />
+        <Route path='/*' element={<Authorization />}>
+          <Route path='home' element={<Home />}/>
         </Route>
       </Routes>
     </div>
